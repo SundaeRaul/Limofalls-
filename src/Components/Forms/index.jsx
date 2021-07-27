ed6bb13554fa20b1e1a48be933a86f7ce50038bc
SundaeRaul <raulrios0907@outlook.com>
2021-07-27 16:39:28 -0300

    forms responsivo

--- src/Components/Forms/index.jsx
+++ src/Components/Forms/index.jsx
@@ -5,19 +5,23 @@ import Background from '../../Imagens/contact-bg.jpg'
 function Forms() {
     return(
         <section className="contact container-fluid m-0 p-0 text-white">
-            <div className="container d-flex flex-column justify-content-around align-items-center position-relative">
-                <h1 className="contact-h1 fw-bold fs-2">Entre em contato</h1>
-                <form className="d-flex flex-column justify-content-around align-items-center">
-                    <input className="inputForm p-2 m-3 w-5" type="text" placeholder="Nome:" />
-                    <input className="inputForm p-2 m-3 w-5" type="text" placeholder="Email:" />
-                    <input className="inputForm p-2 m-3 w-5" type="tel" placeholder="Telefone:" />
-                    <textarea className="inputForm p-3 m-3 w-5" placeholder="Sua Mensagem" />
-                    <button type="button" class="btn p-3 m-5" data-bs-toggle="button" autocomplete="off">Enviar Mensagem</button>
+            <div className="container d-flex flex-column justify-content-around align-items-center">
+                <h1 className="mt-5 fw-bold">Entre em contato</h1>
+                <form className="col-lg-8 col-md-6 d-flex jusify-content-around align-items-center flex-column mt-5">
+                    <label className="p-3 fs-4">Nome Completo</label>
+                    <input className="input-contact p-3 col-lg-12" type="text" placeholder="Seu nome" />
+
+                    <label className="p-3 fs-4">Email</label>
+                    <input className="input-contact p-3 col-lg-12" type="text" placeholder="exemplo@hmail.com"/>
+
+                    <label className="p-3 fs-4">Telefone</label>
+                    <input className="input-contact p-3 col-lg-12" type="tel" placeholder="(45) 99999-9999" />
+
+                    <label className="p-3 fs-4">Sua Mensagem</label>
+                    <textarea className="input-contact p-3 col-lg-12" placeholder="Mensagem" />
                 </form>
-                <div className="d-flex flex-column justify-content-around align-items-center fs-4 fw-bold">
-                    <p>Contato</p>
-                    <p>(45) 99152-1353</p>
-                </div>
+
+                <button className="contact-btn btn col-4 mt-5">Enviar Mensagem</button>
             </div>
         </section>
     )
