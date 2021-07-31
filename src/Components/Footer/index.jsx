f8df1eabfdf1d846593adabfe8415d78fbbebd98
SundaeRaul <raulrios0907@outlook.com>
2021-07-31 13:15:52 -0300

    header responsivo

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './index.css'
 
 function Footer() {
     return(
-        <div className="container-fluid bg-dark text-center">
-              <p className="text-yellow fw-5 fs-2 p-4">COPYRIGHT © LIMOFALLS EXPERIENCE 2021. ALL RIGHTS RESERVED</p>
+        <div className="body-footer container-fluid bg-dark text-center">
+              <p className="text-yellow fw-5 fs-4 p-4">COPYRIGHT © LIMOFALLS EXPERIENCE 2021. ALL RIGHTS RESERVED</p>
         </div>
     )
 }
--- src/Home.js
+++ src/Home.js
@@ -2,20 +2,23 @@ import './Global.css';
 import './Home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './Components/Navbar/index';
-import Header from './Components/Header/index';
-import Section from './Components/Section/index';
-import Forms from './Components/Forms/index';
-import Rodape from './Components/Rodape/index';
 import Footer from './Components/Footer/index';
+import Fundo from './Imagens/fundoLimofalls.jpg'
 
 function Home() {
   return (
-    <div className="container-fluid px-0 d-flex flex-column">
+    <div className="body-home container-fluid px-0 d-flex flex-column">
       <Navigation />
-      <Header />
-      <Section />
-      <Forms />
-      <Rodape />
+      // HEADER
+      <div className="header-container container-fluid m-0 p-0">
+        <div className="container-fluid p-0 m-0">
+          <img src={Fundo} className="fundo-header img-fluid position-absolute" />
+        </div>
+        <div className="text-header container position-relative">
+          <h1 className="h1-header">LIMOFALLS<span className="span-header">EXPERIENCE</span></h1>
+          <h3 className="h3-header">UMA EXPERIÊNCIA INÉDITA NA TERRA DAS CATARATAS</h3>
+        </div>
+      </div>
       <Footer />                    
     </div>
   );
